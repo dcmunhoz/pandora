a6b2750faff93e943cfc18128685a64ce6e5fd1a
dcmunhoz <dc.munhoz@hotmail.com>
2022-12-11 13:22:26 -0300

    feat: Login page

--- frontend/Pandora.Angular/src/app/pages/login/feature/login.component.ts
+++ frontend/Pandora.Angular/src/app/pages/login/feature/login.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private readonly _router: Router) {}
+  protected formLogin: FormGroup;
 
-  public onClick() {
-    this._router.navigateByUrl('signup');
+  constructor(
+    private readonly _router: Router,
+    private readonly _fb: FormBuilder,
+    private readonly _notification: ToastrService
+  ) {
+    this.formLogin = this._fb.group({
+      username: ['', [Validators.required, Validators.maxLength(16)]],
+      password: ['', [Validators.required, Validators.maxLength(64)]]
+    });
+  }
+
+  public login() {
+    if (this.formLogin.invalid) {
+      this._notification.warning('É necessário preencher o usuário e senha', 'Atenção');
+      return;
+    }
   }
 }
--- frontend/Pandora.Angular/src/app/pages/login/login.module.ts
+++ frontend/Pandora.Angular/src/app/pages/login/login.module.ts
@@ -1,10 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 import PButtonModule from 'src/app/common/components/p-button/p-button.module';
+import PInputModule from 'src/app/common/components/p-input/p-input.module';
 import { LoginComponent } from './feature/login.component';
 import { LoginRouterModule } from './login-router.module';
 
 @NgModule({
-  imports: [LoginRouterModule, PButtonModule],
+  imports: [
+    CommonModule,
+    LoginRouterModule,
+    PButtonModule,
+    PInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule
+  ],
   declarations: [LoginComponent],
   exports: []
 })
