a040e88e97dd0b3ad7d347948dc443886fb642ce
dcmunhoz <dc.munhoz@hotmail.com>
2022-12-10 21:46:33 -0300

    feat: Signup page
    Signup base page

--- frontend/Pandora.Angular/src/app/pages/signup/signup.module.ts
+++ frontend/Pandora.Angular/src/app/pages/signup/signup.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SignupRoutingModule } from './signup-routing.module';
 import { SingUpComponent } from './feature/signup.component';
+import PInputModule from 'src/app/common/components/p-input/p-input.module';
+import PButtonModule from 'src/app/common/components/p-button/p-button.module';
 
 @NgModule({
-  imports: [SignupRoutingModule],
+  imports: [SignupRoutingModule, PInputModule, PButtonModule],
   declarations: [SingUpComponent],
   exports: []
 })
